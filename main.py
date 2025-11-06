0d41941f914e0d90d074c42206d25556c1071f6b
323_group <@nomail.ru>
2025-11-06 14:08:20 +0500

    Основная программа для взаимодействия с зависимостями проекта

---
+++ main.py
@@ -0,0 +1,16 @@
+import helper
+
+print("Hello from feature-iln")
+
+while True:
+    choice = int(input("1 – Запустить помощник\n0 – Выйти\n\nВвод: "))
+    if choice == 1:
+        one = int(input("\nПервое: "))
+        two = int(input("Второе: "))
+        r = helper.helper_module(one, two)
+        print(f"\nResult: {r}")
+    elif choice == 0:
+       exit()git
+    else:
+        continue
+   
